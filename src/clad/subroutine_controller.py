9dff9808c4da2fdec4fe5c7698c9bf95689650c2
JuneTech <junetech0@gmail.com>
2025-05-12 09:13:46 +0900

    class SubroutineFlowValidator

--- src/clad/subroutine_controller.py
+++ src/clad/subroutine_controller.py
@@ -6,6 +6,7 @@ from typing import Any, Sequence
 from .dynamic_data_object import DynamicDataObject
 from .elapsed_timer import ElapsedTimer
 from .experiment_summary import ExperimentSummary
+from .subroutine_flow_validator import SubroutineFlowValidator
 
 
 class SubroutineController(ABC):
@@ -14,11 +15,9 @@ class SubroutineController(ABC):
     experiment_summary: ExperimentSummary
 
     stopping_criteria: DynamicDataObject
-    _subroutine_flow: DynamicDataObject
-
-    _working_dir_path: Path
-
-    _method_call_logs: list[dict[str, Any]]
+    __subroutine_flow: DynamicDataObject
+    __method_call_log: list[dict[str, Any]]
+    """A list of dictionaries containing method call logs."""
 
     def __init__(
         self,
@@ -27,7 +26,11 @@ class SubroutineController(ABC):
         subroutine_flow: DynamicDataObject,
         start_dt: datetime | None = None,
     ):
-        # Set the timer first
+        # Check the validity of subroutine flow
+        validator = SubroutineFlowValidator(self.__class__)
+        validator.validate(subroutine_flow)
+
+        # Set the timer
         self.timer = ElapsedTimer()
         if start_dt is not None:
             self.timer.set_start_time(start_dt)
@@ -38,28 +41,15 @@ class SubroutineController(ABC):
         self.experiment_summary = ExperimentSummary(name)
 
         self.stopping_criteria = stopping_criteria
-        self._subroutine_flow = subroutine_flow
-
-        self._method_call_logs = []
-
-    @abstractmethod
-    def is_stopping_condition(self) -> bool:
-        pass
-
-    def _add_method_call_log_entry(self, **kwargs):
-        self._method_call_logs.append(kwargs)
+        self.__subroutine_flow = subroutine_flow
+        self.__method_call_log = []
 
     def set_working_dir(self, dir_path: str):
-        self._working_dir_path = Path(dir_path)
+        self.__working_dir_path = Path(dir_path)
 
     def run(self):
-        self.execute_routine(self._subroutine_flow)
+        self.execute_routine(self.__subroutine_flow)
         self.post_run_process()
-
-    @abstractmethod
-    def post_run_process(self):
-        """Post-process the results after running the subroutine flow."""
-        pass
 
     def execute_routine(self, routine_data: DynamicDataObject):
         if isinstance(routine_data, Sequence):  # is a list or tuple
@@ -93,7 +83,7 @@ class SubroutineController(ABC):
             getattr(self, method_name)(**kwargs)
         except Exception as e:
             print(f"[Error] Method {method_name} failed: {e}")
-            self._add_method_call_log_entry(
+            self.__add_method_call_log_entry(
                 method_name=method_name,
                 start_sec=method_start_sec,
                 elapsed_sec=0,
@@ -103,12 +93,32 @@ class SubroutineController(ABC):
             raise
 
         elapsed_sec = self.timer.get_elapsed_sec() - method_start_sec
-        self._add_method_call_log_entry(
+        self.__add_method_call_log_entry(
             method_name=method_name,
             start_sec=method_start_sec,
             elapsed_sec=elapsed_sec,
             kwargs=kwargs,
         )
+
+    def __add_method_call_log_entry(self, **kwargs):
+        self.__method_call_log.append(kwargs)
+
+    def get_method_call_log(self) -> list[dict[str, Any]]:
+        """Get the method call log.
+
+        Returns:
+            list[dict[str, Any]]: A list of dictionaries containing method call logs.
+        """
+        return self.__method_call_log.copy()
+
+    @abstractmethod
+    def is_stopping_condition(self) -> bool:
+        pass
+
+    @abstractmethod
+    def post_run_process(self):
+        """Post-process the results after running the subroutine flow."""
+        pass
 
     def repeat(self, n_repeats: int, routine_data: DynamicDataObject):
         """
@@ -122,9 +132,9 @@ class SubroutineController(ABC):
         for i in range(n_repeats):
             if self.is_stopping_condition():
                 print(
-                    f"[Repeat] Stopping condition met at iteration {i+1}/{n_repeats}."
+                    f"[Repeat] Stopping condition met at iteration {i + 1}/{n_repeats}."
                 )
                 break
-            print(f"[Repeat] Starting repeat {i+1}/{n_repeats}")
+            print(f"[Repeat] Starting repeat {i + 1}/{n_repeats}")
             ddo = DynamicDataObject.from_obj(routine_data)
             self.execute_routine(ddo)
--- tests/test_subroutine_controller.py
+++ tests/test_subroutine_controller.py
@@ -32,9 +32,10 @@ def sample_controller():
 
 def test_execute_routine(sample_controller: SubroutineController):
     sample_controller.run()
-    assert len(sample_controller._method_call_logs) == 2
-    assert sample_controller._method_call_logs[0]["method_name"] == "sample_method"
-    assert sample_controller._method_call_logs[1]["kwargs"] == {
+    method_call_log = sample_controller.get_method_call_log()
+    assert len(method_call_log) == 2
+    assert method_call_log[0]["method_name"] == "sample_method"
+    assert method_call_log[1]["kwargs"] == {
         "param1": 3,
         "param2": 4,
     }
@@ -42,14 +43,35 @@ def test_execute_routine(sample_controller: SubroutineController):
 
 def test_call_method(sample_controller: SubroutineController):
     sample_controller.call_method("sample_method", param1=10, param2=20)
-    assert len(sample_controller._method_call_logs) == 1
-    assert sample_controller._method_call_logs[0]["method_name"] == "sample_method"
-    assert sample_controller._method_call_logs[0]["kwargs"] == {
+    method_call_log = sample_controller.get_method_call_log()
+    assert len(method_call_log) == 1
+    assert method_call_log[0]["method_name"] == "sample_method"
+    assert method_call_log[0]["kwargs"] == {
         "param1": 10,
         "param2": 20,
     }
 
 
-def test_repeat(sample_controller: SubroutineController):
-    sample_controller.repeat(3, sample_controller._subroutine_flow)
-    assert len(sample_controller._method_call_logs) == 6  # 2 methods * 3 repeats
+@pytest.fixture
+def sample_repeat_controller():
+    stopping_criteria = DynamicDataObject({"stop": False})
+    subroutine_flow = DynamicDataObject.from_obj(
+        {
+            "method_name": "repeat",
+            "n_repeats": 3,
+            "routine_data": [
+                {"method_name": "sample_method", "param1": 1, "param2": 2},
+                {"method_name": "sample_method", "param1": 3, "param2": 4},
+            ],
+        }
+    )
+    controller = MockSubroutineController(
+        "MockSubroutineController", stopping_criteria, subroutine_flow
+    )
+    return controller
+
+
+def test_repeat(sample_repeat_controller: SubroutineController):
+    sample_repeat_controller.run()
+    method_call_log = sample_repeat_controller.get_method_call_log()
+    assert len(method_call_log) == 7  # 1(repeat) + 2 methods * 3 repeats
