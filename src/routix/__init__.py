aae10e25bba4d86a5bb850fa310d419c4be7621b
JuneTech <junetech0@gmail.com>
2025-07-03 11:43:59 +0900

    Remove SolverStatus class and update imports in __init__.py to include StoppingCriteria

--- src/routix/__init__.py
+++ src/routix/__init__.py
@@ -1,6 +1,6 @@
 from .dynamic_data_object import DynamicDataObject, DynamicDataObjectT
 from .elapsed_timer import ElapsedTimer
-from .solver_status import SolverStatus
+from .stopping_criteria import StoppingCriteria, StoppingCriteriaT
 from .subroutine_controller import SubroutineController, SubroutineControllerT
 from .subroutine_flow_validator import SubroutineFlowValidator
 
@@ -8,8 +8,9 @@ __all__ = [
     "DynamicDataObject",
     "DynamicDataObjectT",
     "ElapsedTimer",
-    "SolverStatus",
     "SubroutineController",
     "SubroutineControllerT",
     "SubroutineFlowValidator",
+    "StoppingCriteria",
+    "StoppingCriteriaT",
 ]
